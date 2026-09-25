a7a916ae74298c59ed5fada9c3aeaa13c06f8409
josedavidmarco <josedmarco@gmail.com>
2026-09-25 15:12:20 +0200

    Add files via upload

---
+++ contenidos.js
@@ -0,0 +1,16 @@
+var DATOS_CONTENIDOS = {
+  "items": [
+    {
+      "type": "news",
+      "image": "imagenes/noticia1.jpg", // Asegúrate de que este archivo existe
+      "kicker": "ACTUALIDAD",
+      "title": "Título de la noticia de prueba",
+      "subtitle": "Este es el subtítulo o descripción corta de la noticia.",
+      "duration": 8000
+    },
+    {
+      "type": "video",
+      "src": "videos/corporativo.mp4" // Asegúrate de que este archivo existe
+    }
+  ]
+};
